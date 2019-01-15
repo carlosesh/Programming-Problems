4444bda14c6dedeb44caa788d6ebc9ad650dd47c
Carlos <carlosesh@outlook.com>
2019-01-14 21:43:18 -0600

    SimpleProduct

---
+++ UriOnlineJudge/SimpleProduct.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class SimpleProduct {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long x = sc.nextLong();
+        long y = sc.nextLong();
+        System.out.println("PROD = "+ product(x,y));
+    }
+
+    private static long product(long x, long y){
+        return x*y;
+    }
+}
